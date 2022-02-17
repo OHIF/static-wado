95606d256cd5214f8916ab9492042ceeac235c15
Bill Wallace <wayfarer3130@gmail.com>
2022-02-17 09:28:03 -0500

    Added a functional study query demonstration plugin

--- packages/static-wado-util/lib/index.js
+++ packages/static-wado-util/lib/index.js
@@ -3,6 +3,7 @@ const { Stats } = require("./stats");
 exports.handleHomeRelative = require("./handleHomeRelative");
 exports.JSONReader = require("./reader/JSONReader");
 exports.dirScanner = require("./reader/dirScanner");
+exports.qidoFilter = require("./qidoFilter");
 
 exports.configureProgram = configureProgram;
 exports.program = program;
--- packages/static-wado-webserver/lib/studyQueryReadIndex.mjs
+++ packages/static-wado-webserver/lib/studyQueryReadIndex.mjs
@@ -1,12 +1,31 @@
 import ConfigPoint from "config-point";
+import { JSONReader, qidoFilter } from "@ohif/static-wado-util";
 
 const { studyQueryReadIndex } = ConfigPoint.register({
   studyQueryReadIndex: {
     createWebQuery(directory, params) {
-      console.log("Registering study query read index on", params.rootDir);
+      let studiesData;
+      const dataTime = Date.now();
+      const dataLifetime = params.studyDataLifetime || 1000 * 60;
+      const { rootDir } = params;
+
+      // TODO - use a plugin to read the studies data, to allow switching to a scanner
+      const readStudies = async () => {
+        if (Date.now() - dataTime > dataLifetime) studiesData = null;
+        if (studiesData) return studiesData;
+        studiesData = await JSONReader(rootDir, "studies/index.json.gz");
+        return studiesData;
+      };
 
       return async function webQuery(req, res, next) {
-        console.log("Configured to read from static file location", req.path);
+        const studies = await readStudies();
+        if (studies) {
+          const filteredStudies = qidoFilter(studies, req.query);
+          console.log("Filtered studies count", filteredStudies.length);
+          res.json(filteredStudies);
+          return;
+        }
+        console.log("Couldn't read studies data");
         next();
       };
     },
